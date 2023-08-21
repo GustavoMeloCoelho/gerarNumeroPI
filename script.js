297ee9e3b06d6bcad1ab7f93ac347b23b8a3a1b4
Gustavo  Melo Coelho de Souza <90584080+GustavoMeloCoelho@users.noreply.github.com>
2023-08-21 20:54:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let alphabet = ["A", "B", "C", "D", "E", "F", "H",
+     "L", "M", "N", "P", "E", "S", "T", "U", "V"];
+
+
+function generateNumbers() {
+    let numberToRandom = ((Math.random() * 9999) + 1);
+    let randomNumber = Math.floor(numberToRandom); 
+    return randomNumber;
+};
+
+function generateLetters() {
+    let randomAlphabetNumber = Math.floor(((Math.random() * (alphabet.length -1)) + 1));
+    let randomLetter = alphabet[randomAlphabetNumber];
+    return randomLetter; 
+};
+
+function generatePiNumber() {
+    let number = generateLetters()+generateNumbers();
+    return number;
+}; 
+
+console.log(generatePiNumber());
+
+const piNumberContainer = document.getElementById('piNumberContainer');
+
+let piNumberToAdd = document.createElement("p");
+piNumberToAdd.innerHTML = generatePiNumber();
+piNumberContainer.appendChild(piNumberToAdd);
+
+function removePiNumber() {
+    piNumberContainer.removeChild(piNumberToAdd);
+  
+};
+
+function showPiNumber() {
+    removePiNumber();
+    let numbersToAddAgain = generateNumbers();
+    let letterToAddAgain = generateLetters();
+    let piNumberToAddAgain = numbersToAddAgain + letterToAddAgain;
+    piNumberToAdd = document.createElement("p");
+    piNumberToAdd.innerHTML = piNumberToAddAgain;
+    piNumberContainer.appendChild(piNumberToAdd);
+};
+
+
+
